e6d4d46f40504dd2be9c33f96b0d215c44e28690
Henry Garcia <hgarc014@ucr.edu>
2015-01-25 10:49:30 -0800

    now saving tweets information

--- crawler/src/crawler/main.java
+++ crawler/src/crawler/main.java
@@ -1,21 +1,28 @@
 package crawler;
 
+import twitter4j.HashtagEntity;
+import twitter4j.JSONException;
+import twitter4j.JSONObject;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
+import twitter4j.UserMentionEntity;
 import twitter4j.auth.AccessToken;
 import twitter4j.conf.ConfigurationBuilder;
-import twitter4j.*;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.jar.Attributes.Name;
 
 public class main {
+	
+	static String saveFile = "/home/henry/Desktop/tweets.txt";
 
 	public static void main(String[] args) {
 
@@ -23,7 +30,6 @@ public class main {
 		String accTokensec = "pQfusO6QK6D8TwkN1MYorMjJWl2brx6fSj6CSfynK0Asw";
 		String consumer = "GQMH51Jbw075KnlYrcgSqPjhb";
 		String consumersec = "DCKu6VdwUgokz8drPmWqR6mFTBeDc6yyd7eoDMU23u0kUcYxm9";
-
 
 		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
 		AccessToken ac = new AccessToken(accToken, accTokensec);
@@ -33,7 +39,13 @@ public class main {
 			@Override
 			public void onStatus(Status status) {
 
-				printInformation(status);
+				 try {
+					saveTweet(status);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+//				printInformation(status);
 			}
 
 			@Override
@@ -73,23 +85,76 @@ public class main {
 		char[] charArray = new char[20];
 		Arrays.fill(charArray, '-');
 		String line = new String(charArray);
-		System.out.println(line + "\nPostID: " + status.getId()+ "\n@" + status.getUser().getScreenName()
-				+ "\nTimeZone: " + status.getUser().getTimeZone()
-				+ "\nUserLocation: " + status.getUser().getLocation()
-				+ "\nFriends: " + status.getUser().getFriendsCount()
-				+ "\nFollowers: " + status.getUser().getFollowersCount()
-				+ "\nGeoLocation: " + status.getGeoLocation());
-		System.out.println("\nURLS: ");
-		for (URLEntity e : status.getURLEntities()) {
-			System.out.println("Display: " + e.getDisplayURL() + "\nExpanded: "
-					+ e.getExpandedURL() + "\nURL: " + e.getURL());
-		}
+		System.out.println(line + "\nUSER INFORMATION" + "\n@"
+				+ status.getUser().getScreenName() + "\nTimeZone: "
+				+ status.getUser().getTimeZone() + "\nUserLocation: "
+				+ status.getUser().getLocation() + "\nFriends: "
+				+ status.getUser().getFriendsCount() + "\nFollowers: "
+				+ status.getUser().getFollowersCount()
+				+ "\nProfileDescription: " + status.getUser().getDescription()
+				+ "\nStatus: " + status.getUser().getStatus()
+				+ "\nStatusCount: " + status.getUser().getStatusesCount()
+				+ "\nLanguage: " + status.getUser().getLang());
+
+		System.out.println("\nTWEET INFORMATION" + "\nTweetID: "
+				+ status.getId() + "Language: " + status.getLang()
+				+ "\nGeoLocation: " + status.getGeoLocation() + "\nRetweets: "
+				+ status.getRetweetCount() + "\nFavorites: "
+				+ status.getFavoriteCount() + "\nPlace: " + status.getPlace()
+				+ "\nCreatedAt: " + status.getCreatedAt());
+
 		System.out.println("\nMENTIONS:");
 		for (UserMentionEntity u : status.getUserMentionEntities()) {
 			System.out.println("@" + u.getScreenName());
 		}
+		System.out.println("\nPOUNDSIGNS:");
+		for (HashtagEntity h : status.getHashtagEntities()) {
+			System.out.println("#" + h.getText());
+		}
+		System.out.println("\nBody:\n" + status.getText());
+	}
 
-		System.out.println("\nFavorites: " + status.getFavoriteCount()
-				+ "\nbody:" + status.getText() + "\n");
+	public static JSONObject createJsonObj(Status status) throws JSONException {
+		JSONObject j = new JSONObject();
+
+		//important fields
+		j.put("TweetID", status.getId());
+		j.put("User", status.getUser().getScreenName());
+		j.put("TweetLanguage", status.getLang());
+		j.put("TweetGeoLoc", status.getGeoLocation());
+		j.put("TweetCreationDate", status.getCreatedAt());
+		j.put("Body", status.getText());
+		
+		//optional fields
+		j.put("UserFriends", status.getUser().getFriendsCount());
+		j.put("UserFollowers", status.getUser().getFollowersCount());
+		j.put("UserLocation", status.getUser().getLocation());
+		j.put("UserLanguage", status.getUser().getLang());
+		j.put("UserTimeZone", status.getUser().getTimeZone());
+		j.put("UserDesc", status.getUser().getDescription());
+		j.put("UserStatus", status.getUser().getStatus());
+		j.put("UserStatusCount", status.getUser().getStatusesCount());
+		
+		j.put("Retweets", status.getRetweetCount());
+		j.put("Favorites", status.getFavoriteCount());
+		j.put("TweetPlace", status.getPlace());
+		
+		System.out.println("Created Json Object");
+		return j;
+	}
+
+	public static void saveTweet(Status status) throws IOException {
+		FileWriter file = new FileWriter(saveFile, true);
+		try {
+			JSONObject tweet = createJsonObj(status);
+			file.write(tweet.toString() + "\n");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+//			file.flush();
+			file.close();
+		}
+
 	}
 }
